623b8110f10c6395fa1139b29eca1ce5ab7fce22
JoaoStipanic <joao.salmeida@sptech.school>
2025-12-04 13:52:52 -0300

    Site final

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -2,9 +2,7 @@ var database = require("../database/config");
 
 function ultimas() {
     var instrucaoSql = `
-        SELECT DATE_FORMAT(dataLeitura, '%H') AS hora, FLOOR(MINUTE(dataLeitura)/10)*10 AS dezena_minuto, 
-	    SUM(leitura) as leitura FROM SensorLeitura where date_format(dataLeitura, '%H') = (select date_format(dataLeitura,'%H')
-        from SensorLeitura order by dataLeitura desc limit 1 offset 1) GROUP BY hora, dezena_minuto ORDER BY hora, dezena_minuto ASC; 
+        select * from vw_horas_minutos; 
     `;
 
     console.log("Executando SQL:\n" + instrucaoSql);
